879c2fe2511890be0348c125bd35e4ec313c7f81
John Cinnamond <jc@panagile.com>
2013-01-25 11:01:25 +0000

    New rails 3.2.11 app.
    Generated with: rails new basketball -OT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basketball::Application.config.secret_token = '471725004ea051a21332cae6df97c18cc081a2fb34c90302a61629fbbddd48fbbc0afea28afd02b89384a87f115863da4cd035466cbdafb70f5229a2cc85bc87'
